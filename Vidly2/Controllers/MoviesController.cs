2554bcb2eea1cd0599d3d8f2a1d88fcd3aed00b0
Csaba Erdes <csaba.erdes@gmail.com>
2019-05-04 16:20:34 +0200

    addimg some controllers and models

---
+++ Vidly2/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+using Vidly2.ViewModels;
+
+namespace Vidly2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+
+            var movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+
+            var customers = new List<Customer>()
+            {
+                new Customer { Name = "Alan"},
+                new Customer { Name = "Susan"},
+                new Customer { Name = "George"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}, sortBy={1}", pageIndex,sortBy));
+        }
+    }
+} 
