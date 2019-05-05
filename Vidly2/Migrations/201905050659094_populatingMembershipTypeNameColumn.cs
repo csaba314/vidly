db6514ae754a8b19ae1369b59f331874dbdea379
Csaba Erdes <csaba.erdes@gmail.com>
2019-05-05 09:07:11 +0200

    adding name prop to MembershipType and updating the db with data

---
+++ Vidly2/Migrations/201905050659094_populatingMembershipTypeNameColumn.cs
@@ -0,0 +1,29 @@
+namespace Vidly2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class populatingMembershipTypeNameColumn : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes " +
+                " SET Name = 'PayAsYouGo' " +
+                " WHERE Id = 1");
+            Sql("UPDATE MembershipTypes " +
+                " SET Name = 'Monthly' " +
+                " WHERE Id = 2");
+            Sql("UPDATE MembershipTypes " +
+                " SET Name = 'Quaterly' " +
+                " WHERE Id = 3");
+            Sql("UPDATE MembershipTypes " +
+                " SET Name = 'Anualy' " +
+                " WHERE Id = 4");
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
--- Vidly2/Models/MembershipType.cs
+++ Vidly2/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,14 @@ namespace Vidly2.Models
     public class MembershipType
     {
         public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
         public int SignUpFee { get; set; }
+
         public int DurationInMonths { get; set; }
+
         public int DiscountRate { get; set; }
     }
 }
