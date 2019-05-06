83538a957b42dc4ca03d2fac3315269f9208e4ef
Csaba Erdes <csaba.erdes@gmail.com>
2019-05-06 08:48:13 +0200

    adding display name data annotations to movie model

--- Vidly2/Models/Movie.cs
+++ Vidly2/Models/Movie.cs
@@ -18,12 +18,15 @@ namespace Vidly2.Models
         public Genre Genre { get; set; }
 
         [Required]
+        [Display(Name ="Genre")]
         public int GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name ="Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name="Number in Stock")]
         public int NumberInStock { get; set; }
 
     }
