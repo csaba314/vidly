f727049e470fff26c992cfc9ac93de3eb8036a88
Csaba Erdes <csaba.erdes@gmail.com>
2019-05-09 17:54:05 +0200

    adding api MoviesController and update to mapping profile for automapper

--- Vidly2/App_Start/MappingProfile.cs
+++ Vidly2/App_Start/MappingProfile.cs
@@ -13,7 +13,14 @@ namespace Vidly2.App_Start
         public MappingProfile()
         {
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore()); // to fix the Id bug
+
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
---
+++ Vidly2/Controllers/Api/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly2.Models;
+using Vidly2.Dtos;
+using AutoMapper;
+
+namespace Vidly2.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/movies/
+        public IHttpActionResult GetMovies()
+        {
+            var movieDtos = _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+
+            return Ok(movieDtos);
+        }
+    }
+}
