045bbbfbecbf5cf5ee9ee7860648f738af02a7c4
Csaba Erdes <csaba.erdes@gmail.com>
2019-05-13 17:16:31 +0200

    adding RequireHttpsAttribute to FilterConfig class

--- Vidly2/App_Start/FilterConfig.cs
+++ Vidly2/App_Start/FilterConfig.cs
@@ -10,6 +10,9 @@ namespace Vidly2
             filters.Add(new HandleErrorAttribute());
             // adding a global [Authorize] atribute
             filters.Add(new AuthorizeAttribute());
+
+            // adding a new filter to stop access to the app on http channel
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
