017bf8d22b874a5d2b7a36cb043e4051d218be15
Csaba Erdes <csaba.erdes@gmail.com>
2019-05-14 09:44:57 +0200

    extracting ExternalLoginConfirmationView to a separate file and adding the Phone property

--- Vidly2/Models/AccountViewModels.cs
+++ Vidly2/Models/AccountViewModels.cs
@@ -3,18 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Vidly2.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-
-        // add DrivingLicence prop to the ExternalLoginConfirmatioViewModel 
-        // so external authentication does not fail
-        [Required]
-        [Display(Name = "Driving License")]
-        public string DrivingLicense { get; set; }
-    }
 
     public class ExternalLoginListViewModel
     {
---
+++ Vidly2/Models/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly2.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        // add DrivingLicence prop to the ExternalLoginConfirmatioViewModel 
+        // so external authentication does not fail
+        [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
+        // add Phone prop to the ExternalLoginConfirmationViewModel so we can use it 
+        // for Social logins
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
+    }
+}
