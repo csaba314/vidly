89f90b777817eb115393b2469ef447281e06bb43
Csaba Erdes <csaba.erdes@gmail.com>
2019-05-15 08:42:23 +0200

    OutputCache action filter

--- Vidly2/Controllers/HomeController.cs
+++ Vidly2/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+// using System.Web.UI; to access enum OutputCacheLocation
+using System.Web.UI;
 
 namespace Vidly2.Controllers
 {
@@ -11,6 +13,17 @@ namespace Vidly2.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        // action filter, can be set to the whole controller or individual actions
+        /*
+         * [OutputCache(Duration = 50, Location = OutputCacheLocation.Server, VaryByParam ="*")]
+         * 
+        [OutputCache(
+            Duration = 50,  // duration of the cache set in seconds 
+            Location = OutputCacheLocation.Server,  // sets the location of the cache on the server
+            VaryByParam ="*")]  // ="paramName" makes a separate cache for every param of that type
+                            // =" * " makes a separate cache for every param variation
+        */
+        // to disable output caching [OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)]
         public ActionResult Index()
         {
             return View();
