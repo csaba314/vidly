9333ecbb6c74b6fe14d21cd78e486221cc9e5f90
Csaba Erdes <csaba.erdes@gmail.com>
2019-06-03 10:15:46 +0200

    adding Rentals.cs to model, adding DbSet<Rental> to ApplicationDbContext.cs and migrating to DB

--- Vidly2/Models/ApplicationDbContext.cs
+++ Vidly2/Models/ApplicationDbContext.cs
@@ -5,11 +5,12 @@ namespace Vidly2.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-
+        // add a DbSet for a model class so EF can add it to a migration
         public DbSet<Customer> Customers{ get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<MembershipType> MembershipTypes { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
 
 
         public ApplicationDbContext()
